a52461565e9824b8f3b0436bd6f5c0a4a8f05720
andrayantelo <andrayantelo@gmail.com>
2020-03-06 10:44:58 -0800

    changed alert

---
+++ public/js/main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// Get the parameter from the URL
+// Disable linter for this line because web environment
+// isn't disabling node checks
+const urlParams = new URLSearchParams(window.location.search);	// eslint-disable-line node/no-unsupported-features/node-builtins
+const msg = urlParams.get('message');
+const messageDiv = document.getElementById('message-div');
+messageDiv.innerText = msg;
+if (msg) {
+	messageDiv.style.display = 'block';
+	setTimeout(() => {
+		messageDiv.style.display = 'none';
+	}, 3000);
+}
