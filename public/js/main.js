e9aef5fd1f0d06f80064bcd905d524f11749339a
andrayantelo <andrayantelo@gmail.com>
2020-03-06 11:19:00 -0800

    fixed eslint stuff

--- public/js/main.js
+++ public/js/main.js
@@ -1,21 +1,22 @@
 'use strict';
+
 // Alert user about comment submission success
 // Get the parameter from the URL
 // Disable linter for this line because web environment
 // isn't disabling node checks
 const urlParams = new URLSearchParams(window.location.search);	// eslint-disable-line node/no-unsupported-features/node-builtins
 const msg = urlParams.get('message');
-const modal = document.querySelector(".modal");
-function toggleModal() {
-	modal.classList.toggle("show-modal");
-}
-function windowOnClick(event) {
+const modal = document.querySelector('.modal');
+const toggleModal = () => {
+	modal.classList.toggle('show-modal');
+};
+const windowOnClick = (event) => {
 	if (event.target === modal) {
 		toggleModal();
 	}
-}
+};
 if (msg) {
-  window.addEventListener("click", windowOnClick);
-  document.getElementById("modal-text").innerText = msg;
-  toggleModal();
+	window.addEventListener('click', windowOnClick);
+	document.getElementById('modal-text').innerText = msg;
+	toggleModal();
 }
