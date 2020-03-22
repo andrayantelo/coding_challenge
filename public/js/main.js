5f42eeac2e40e51c8238699c09a053644b5d4446
andrayantelo <andrayantelo@gmail.com>
2020-03-21 18:47:31 -0700

    Wrote own submit handler function. Use fetch to send request.

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,4 @@
 'use strict';
-
-// Handler for submitting a comment
-const commentForm = document.getElementById('commentForm');
-
-const onSubmit = (e) => {
-	e.preventDefault();
-	//fetch('/addComment')
-}
-commentForm.addEventListener('submit', onSubmit);
-
 // Alert user about comment submission success
 // Get the parameter from the URL
 // Disable linter for this line because web environment
@@ -24,8 +14,31 @@ const windowOnClick = (event) => {
 		toggleModal();
 	}
 }
-if (msg) {
-	window.addEventListener('click', windowOnClick);
-	document.getElementById('modal-text').innerText = msg;
-	toggleModal();
-}
+window.addEventListener('click', windowOnClick);
+// Handler for submitting a comment
+const commentForm = document.getElementById('commentForm');
+
+commentForm.addEventListener('submit', function (e) {
+	e.preventDefault();
+	const formData = new FormData(this);
+	const searchParams = new URLSearchParams();
+
+	for (const pair of formData) {
+		searchParams.append(pair[0], pair[1]);
+	}
+	
+	fetch('/addComment', {
+		method: 'POST',
+		body: searchParams
+	}).then(function (response) {
+		return response.text();
+	}).then(function (msg) {
+		if (msg) {
+			document.getElementById('modal-text').innerText = msg;
+			toggleModal();
+		}
+	}).catch(function (error) {
+		console.error(error);
+	})
+});
+
